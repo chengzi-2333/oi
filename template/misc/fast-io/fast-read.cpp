0a0439df292dbf2c719ac3e40b88a3c0abd22d20
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-09 11:41:24 +0800

    fast-read

---
+++ template/misc/fast-io/fast-read.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// modify reference
+inline void fast_read(int& x) {
+    char c;
+    x = 0;
+    bool neg = false;
+    while (!isdigit(c = getchar_unlocked())) {
+        if (c == '-') neg = true;
+    }
+    do x = x * 10 + (c - '0');
+    while (isdigit(c = getchar_unlocked()));
+    if (neg) x = -x;
+}
+
+// return read value
+inline int fast_read() {
+    char c;
+    int x = 0;
+    bool neg = false;
+    while (!isdigit(c = getchar_unlocked())) {
+        if (c == '-') neg = true;
+    }
+    do x = x * 10 + (c - '0');
+    while (isdigit(c = getchar_unlocked()));
+    return neg ? -x : x;
+}
+
+int n, sum;
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("fast-read.in", "r", stdin);
+#endif // ONLINE_JUDGE
+    n = fast_read();
+    while (n--) sum += fast_read();
+    printf("%d", sum);
+}
