fa82d43d48cb67e1ff4fb66fc6a3ef053a69de53
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-16 21:00:24 +0800

    communication

--- template/misc/communication/communication.cpp
+++ template/misc/communication/communication.cpp
@@ -1,11 +1,47 @@
 // {P12509}
+// Hamming Code
 #include <string>
 
+inline bool is_pw2(int x) {return x && !((x-1) & x);}
+
+inline int fill_buf(const std::string& s, unsigned char* buf) {
+    int k = 0;
+    while ((1 << k) < s.length() + k + 1) k++;
+    int buf_size = (1 << k) - 1;
+    unsigned char* pbuf = buf + 1;
+    for (auto it = s.begin() + 1; pbuf - buf <= buf_size && it < s.end(); pbuf++) {
+        if (!is_pw2(pbuf - buf)) *pbuf = *it++ - '0';
+    }
+    return buf_size;
+}
 
 int Alice(std::string S) {
-    // TODO
+    int cnt[21] = {0};
+    unsigned char buf[1 << 20] = {0};
+    int n = fill_buf(S, buf);
+    for (int i = 1; i <= n; i++) {
+        if (buf[i]) {
+            for (int c = 0, bi = i; bi; bi >>= 1, c++) {
+                if (bi & 1) cnt[c]++;
+            }
+        }
+    }
+    int ret = 0;
+    for (int i = 0; (1 << i) <= n; i++) {
+        ret |= (buf[1 << i] = (cnt[i] & 1)) << i;
+    }
+    return ret;
 }
 
 int Bob(std::string T, int X) {
-    // TODO
+    unsigned char buf[1 << 20] = {0};
+    int n = fill_buf(T, buf);
+    for (int i = 0; (1 << i) <= n; i++) {
+        buf[1 << i] = (X >> i) & 1;
+    }
+    unsigned int pos = 0;
+    for (int i = 1; i <= n; i++) {
+        if (buf[i]) pos ^= i;
+    }
+    return pos ? pos - std::__lg(pos) : 0;
 }
