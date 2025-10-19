5836ca5b71359c2b82959a9c4eea764097beff89
chengzi <1450959649@qq.com>
2025-10-19 14:42:26 +0800

    mst(prim) & dsu(optimized)

--- template/graph/mst/kruskal.cpp
+++ template/graph/mst/kruskal.cpp
@@ -9,8 +9,6 @@
 #include <algorithm>
 
 
-typedef std::tuple<int, int, int> Triple;
-
 char* rp;
 
 inline void fast_read(int& x) {
@@ -26,34 +24,28 @@ inline void fast_read(T& fst, A&... args) {
 	fast_read(fst), fast_read(args...);
 }
 
+inline void mmap_init() {
+	struct stat st;
+	fstat(0, &st);
+	rp = (char*) mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+}
+
 class DSU {
 private:
 	std::vector<int> uni, rank;
 	
 public:
 	DSU(int n) {
-		uni.resize(n+1); rank.resize(n+1, 1);
-		// std::iota(uni.begin(), uni.end(), 0);
+		uni.resize(n+1);
+		rank.resize(n+1);
 	}
-
-#ifdef DSU_RECURSIVE
-	// recursive
+	
 	int root(int x) {
-		// return (uni[x] == x) ? x : uni[x] = root(uni[x]);
 		return uni[x] ? uni[x] = root(uni[x]) : x;
 	}
-#else
-	// loop-based
-	int root(int x) {
-		int t = x, nt = t;
-		while (uni[x]) x = uni[x];
-		while (uni[t]) nt = uni[t], uni[t] = x, t = nt;
-		return x;
-	}
-#endif
-
+	
 	void insert(int x, int y) {
-        x = root(x), y = root(y);
+		x = root(x), y = root(y);
         if (x == y) return;
         if (rank[y] < rank[x]) std::swap(x, y);
         if (rank[x] == rank[y]) rank[y] = rank[x]+1;
@@ -63,47 +55,35 @@ public:
 	bool related(int x, int y) {
 		return root(x) == root(y);
 	}
-	
-	int count() {
-		int s=0;
-		for (int i=1; i<(int) uni.size(); i++)
-			if (!uni[i])
-				s++;
-		return s;
-	}
 };
 
-int n, m;
-std::vector<Triple> edges;
 
-int kruskal() {
-    DSU dsu(n);
-    int ans = 0;
+int n, m;
+std::vector<std::tuple<int, int, int>> edges;
+
+inline int kruskal() {
+	DSU dsu(n);
+    int tot = 0, ans = 0;
+	std::sort(edges.begin(), edges.end());
     for (const auto& [w, u, v]: edges) {
         if (dsu.related(u, v)) continue;
         dsu.insert(u, v);
         ans += w;
+		tot++;
+		if (tot == n - 1) break;
     }
-    return ans;
+    return (tot == n - 1) ? ans : -1;
 }
 
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("mst.in", "r", stdin);
 #endif
-	struct stat st;
-	fstat(0, &st);
-	rp = (char*) mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+	mmap_init();
 	fast_read(n, m);
     edges.resize(m);
-    DSU dsu(n);
-    for (auto& [w, u, v]: edges) {
-		fast_read(u, v, w);
-        dsu.insert(u, v);
-    }
-    if (dsu.count() != 1) puts("orz");
-	else {
-    	std::sort(edges.begin(), edges.end());
-		printf("%d\n", kruskal());
-	}
+    for (auto& [w, u, v]: edges) fast_read(u, v, w);
+	int ans = kruskal();
+    if (ans == -1) puts("orz");
+	else printf("%d\n", ans);
 }
