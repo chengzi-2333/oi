e6792e840dd278aeb54f68f1ebd7f95a069a89dc
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-21 14:52:05 +0800

    simulation/dl24oj/10-18/bus

--- test/simulation/dl24oj/10-18/bus/bus.cpp
+++ test/simulation/dl24oj/10-18/bus/bus.cpp
@@ -15,22 +15,48 @@ inline void fast_read(T& x, A&... a) {
 }
 
 
-constexpr int N = 1e3, M = 1e4;
-struct plan {
-    int t, u, v;
-
-    bool operator <(const plan& p) const {
-        return u < p.u;
-    }
-} p[M+5];
-int n, m, k, d[N+5];
+int n, m, k, ans;
+std::vector<int> dis, sum, lst, arr, pre;
+std::vector<std::tuple<int, int, int>> plan;
 
 int main() {
     freopen("bus.in", "r", stdin);
     freopen("bus.out", "w", stdout);
     fast_read(n, m, k);
-    for (auto dp=d+1; dp-d<n; dp++) fast_read(*dp);
-    for (auto pp=p+1; pp-p<=n; pp++) fast_read(pp->t, pp->u, pp->v);
-    std::sort(p+1, p+1+n);
-    printf("%d\n", std::accumulate(d+1, d+n+1, 0));
+
+    plan.resize(m), dis.resize(n);
+    sum.resize(n+1), lst.resize(n+1), arr.resize(n+1);
+    pre.resize(n);
+
+    for (auto d=dis.begin()+1; d<dis.end(); d++) fast_read(*d);
+    for (auto& [t, u, v]: plan) {
+        fast_read(t, u, v);
+        sum[v]++;
+        lst[u] = std::max(lst[u], t);
+    }
+    std::partial_sum(sum.begin()+1, sum.end(), sum.begin()+1);
+    
+    for (int i=1; i<=n; i++) 
+        arr[i] = std::max(arr[i-1], lst[i-1]) + dis[i-1];
+    for (const auto& [t, u, v]: plan) ans += arr[v] - t;
+
+    while (k--) {
+        pre[n-1] = n;
+        for (int i=n-2; i; i--) {
+            pre[i] = (arr[i+1]>lst[i+1]) ? pre[i+1] : i+1;
+        }
+        int ma = 0, pos = 0;
+        for (int i=1; i<n; i++) {
+            if (dis[i] > 0 && sum[pre[i]] - sum[i] > ma) {
+                ma = sum[pre[i]] - sum[i];
+                pos = i;
+            }
+        }
+        ans -= ma;
+        dis[pos]--;
+        for (int i=pos+1; i<=n; i++) 
+            arr[i] = std::max(arr[i-1], lst[i-1]) + dis[i-1];
+    }
+    
+    std::cout << ans << std::endl;
 }
