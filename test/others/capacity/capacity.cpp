97f0e2f85059d82577ca7cdee5147681c0855e01
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-24 14:11:16 +0800

    capacity

--- test/others/capacity/capacity.cpp
+++ test/others/capacity/capacity.cpp
@@ -1,38 +1,43 @@
 #include <bits/stdc++.h>
 
 
-int n, ma, mi = INT_MAX, fac;
+inline void output(int a, int b) {
+    if ((a < 0) ^ (b < 0)) std::cout << '-';
+    a = std::abs(a), b = std::abs(b);
+    int fac = std::__gcd(a, b);
+    a /= fac, b /= fac;
+    std::cout << a;
+    if (b != 1) std::cout << '/' << b;
+    std::cout << std::endl;
+}
+
+int n, ma = INT_MIN, mi = INT_MAX, nma = INT_MIN, nmi = INT_MAX;
 
 signed main() {
 #ifndef ONLINE_JUDGE
-    // freopen("capacity.in", "r", stdin);
+    freopen("capacity4.in", "r", stdin);
 #endif // ONLINE_JUDGE
     std::cin >> n;
     if (n == 2) {
         std::cin >> ma >> mi;
-        ma = std::abs(ma), mi = std::abs(mi);
-        if (ma < mi) std::swap(ma, mi);
-        fac = std::__gcd(ma, mi);
-        ma /= fac, mi /= fac;
-        std::cout << ma;
-        if (mi != 1) std::cout << '/' << mi;
-        std::cout << std::endl;
-        // TODO: negative
-    }
-    else if (n <= 3000) {
+        if (std::abs(ma) < std::abs(mi) && !((ma < 0) ^ (mi < 0))) 
+            std::swap(ma, mi);
+        output(ma, mi);
+    } else {
         for (int v; n; n--) {
             std::cin >> v;
-            ma = std::max(ma, std::abs(v));
-            mi = std::min(mi, std::abs(v));
+            if (v > 0) {
+                ma = std::max(ma, v);
+                mi = std::min(mi, v);
+            } else {
+                nma = std::max(nma, v);
+                nmi = std::min(nmi, v);
+            }
         }
-        if (ma < mi) std::swap(ma, mi);
-        fac = std::__gcd(ma, mi);
-        ma /= fac, mi /= fac;
-        std::cout << ma;
-        if (mi != 1) std::cout << '/' << mi;
-        std::cout << std::endl;
-    }
-    else {
-        // TODO
+        if ((nma == INT_MIN) || (nmi == INT_MAX)) output(ma, mi);
+        else if ((ma == INT_MIN) || (mi == INT_MAX)) output(nmi, nma);
+        // a/b > c/d => a*d > c*b
+        else if (ma * -nma > mi * -nmi) output(ma, mi);
+        else output(nmi, nma);
     }
 }
