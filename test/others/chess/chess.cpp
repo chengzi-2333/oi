5051ca8240bd3c073ec10f45923c07b7b3a21871
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-31 12:59:01 +0800

    chess(TODO)

---
+++ test/others/chess/chess.cpp
@@ -0,0 +1,12 @@
+// {P5182}
+#include <bits/stdc++.h>
+
+int n, m;
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("chess.in", "r", stdin);
+#endif  // ONLINE_JUDGE
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cin >> n >> m;
+}
