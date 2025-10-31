2b905a7616eb5e37acc4d886ca83b4e0d56c0d9d
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-10-31 13:23:32 +0800

    maximum-matching-bipartile(TODO)

---
+++ template/graph/matching/maximum-matching/maximum-matching-bipartile.cpp
@@ -0,0 +1,13 @@
+// {P3386}
+#include <bits/stdc++.h>
+
+int n, m, e;
+std::vector<std::vector<int>> g;
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("maximum-matching-bipartile.in", "r", stdin);
+#endif  // ONLINE_JUDGE
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cin >> n >> m >> e;
+}
