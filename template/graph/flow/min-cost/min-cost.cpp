b0e67d04f6403519b11c0d5c5d64f345f70c9376
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-11-05 16:34:29 +0800

    graph/flow

---
+++ template/graph/flow/min-cost/min-cost.cpp
@@ -0,0 +1,10 @@
+// {P3381}
+#include <bits/stdc++.h>
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("min-cost.in", "r", stdin);
+#endif  // ONLINE_JUDGE
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    // TODO
+}
---
+++ template/graph/flow/stoer-wagner/stoer-wagner.cpp
@@ -0,0 +1,10 @@
+// {P5632}
+#include <bits/stdc++.h>
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("stoer-wagner.in", "r", stdin);
+#endif  // ONLINE_JUDGE
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    // TODO
+}
