1796fd12ccff482550775bfa09d41ca2cab2a375
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-11-14 20:57:45 +0800

    01-bfs(TODO)

---
+++ template/graph/01-bfs/01-bfs.cpp
@@ -0,0 +1,54 @@
+// {P4667}
+#include <bits/stdc++.h>
+
+constexpr int INF = 0x3f3f3f3f;
+int n, m;
+std::vector<std::vector<bool>> g;
+
+inline bool check(int x, int y) {
+    return x >= 0 && y >= 0 && x < n && y < m;
+}
+
+int bfs() {  // TODO
+    std::vector<std::vector<int>> dis(n, std::vector<int>(m, INF));
+    std::vector<std::vector<bool>> vis(n, std::vector<bool>(m));
+    std::deque<std::pair<int, int>> que;
+    que.emplace_front(0, 0);
+    while (!que.empty()) {
+        const auto [x, y] = que.front();
+        que.pop_front();
+        if (vis[x][y]) continue;
+        vis[x][y] = true;
+        for (const auto& dx : {1, -1}) {
+            for (const auto& dy : {1, -1}) {
+                if (check(x + dx, y + dy)) {
+                    if (g[x][y] ^ g[x + dx][y + dy]) {
+                        que.emplace_back(x + dx, y + dy);
+                        dis[x + dx][y + dy] = dis[x][y] + 1;
+                    } else {
+                        que.emplace_front(x + dx, y + dy);
+                        dis[x + dx][y + dy] = dis[x][y];
+                    }
+                }
+            }
+        }
+    }
+    return dis.back().back();
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("01-bfs.in", "r", stdin);
+#endif  // ONLINE_JUDGE
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cin >> n >> m;
+    g.resize(n, std::vector<bool>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char c;
+            std::cin >> c;
+            g[i][j] = (c == '/');
+        }
+    }
+    std::cout << (g.front().front() + bfs() + g.back().back()) << std::endl;
+}
