08787503e03fbfbaef2bcace0a80238778e0a786
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-11-19 16:07:27 +0800

    01-bfs

--- template/graph/01-bfs/01-bfs.cpp
+++ template/graph/01-bfs/01-bfs.cpp
@@ -2,11 +2,11 @@
 #include <bits/stdc++.h>
 
 constexpr int INF = 0x3f3f3f3f;
-std::vector<std::vector<std::pair<int, bool>>> g;
+std::vector<std::vector<std::pair<int, int>>> g;
 
 int bfs(int s, int t) {
     std::vector<int> dis(g.size(), INF);
-    std::vector<bool> vis(g.size());
+    // std::vector<bool> vis(g.size());
     std::deque<int> que;
     que.push_back(s);
     dis[s] = 0;
@@ -14,8 +14,8 @@ int bfs(int s, int t) {
         int u = que.front();
         que.pop_front();
         if (u == t) break;
-        if (vis[u]) continue;
-        vis[u] = true;
+        // if (vis[u]) continue;
+        // vis[u] = true;
         for (const auto& [v, w] : g[u]) {
             if (dis[v] > dis[u] + w) {
                 dis[v] = dis[u] + w;
@@ -29,12 +29,13 @@ int bfs(int s, int t) {
 
 int n, m;
 
-inline bool check(int x, int y) {
-    return x >= 0 && y >= 0 && x < n && y < m;
+inline int state(int x, int y) {
+    return x * (m + 1) + y + 1;
 }
 
-inline int state(int x, int y) {
-    return x * m + y;
+inline void add_edge(int u, int v, int w) {
+    g[u].emplace_back(v, w);
+    g[v].emplace_back(u, w);
 }
 
 signed main() {
@@ -43,24 +44,19 @@ signed main() {
 #endif  // ONLINE_JUDGE
     std::cin.tie(nullptr)->sync_with_stdio(false);
     std::cin >> n >> m;
-    g.resize(n * m);
+    g.resize((n + 1) * (m + 1) + 1);
     for (int x = 0; x < n; x++) {
         for (int y = 0; y < m; y++) {
             char c;
             std::cin >> c;
-            bool flag = (c == '\\');
-            for (const auto& dx : {-1, 1}) {
-                for (const auto& dy : {-1, 1}) {
-                    int tx = x + dx, ty = y + dy;
-                    bool w = flag ^ (dx * dy > 0);
-                    if (!check(tx, ty)) continue;
-                    g[state(x, y)].emplace_back(state(tx, ty), w);
-                    g[state(tx, ty)].emplace_back(state(x, y), w);
+            for (const auto& dx : {0, 1}) {
+                for (const auto& dy : {0, 1}) {
+                    add_edge(state(x + dx, y + dy), state(x + !dx, y + !dy), (c == '\\') ^ (dx == dy));
                 }
             }
         }
     }
-    auto sol = bfs(state(0, 0), state(n - 1, m - 1));
+    auto sol = bfs(state(0, 0), state(n, m));
     if (sol == INF) std::cout << "NO SOLUTION";
     else std::cout << sol;
     std::cout << std::endl;
