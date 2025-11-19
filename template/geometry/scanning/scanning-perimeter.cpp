0d2ee2bff532c06d94e470c2d089fd2ffe570d03
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-11-19 19:49:59 +0800

    scanning-perimeter

--- template/geometry/scanning/scanning-perimeter.cpp
+++ template/geometry/scanning/scanning-perimeter.cpp
@@ -1,65 +1,99 @@
 // {P1856}
 #include <bits/stdc++.h>
 
-using namespace std;
+struct SegmentTree {
+    using tree_ptr = std::unique_ptr<SegmentTree>;
 
-// discrete
-vector<int> disc(1);
+    int lb, rb;
+    int key = 0, tag = 0;
+    tree_ptr left = nullptr, right = nullptr;
 
-int pos(int val) {
-    return lower_bound(disc.begin(), disc.end(), val)-disc.begin();
-}
+    inline int middle(int l, int r) { return l + ((r - l) >> 1); }
 
-void discrete() {
-    sort(disc.begin()+1, disc.end());
-    disc.erase(unique(disc.begin()+1, disc.end()), disc.end());
-}
+    inline int middle() { return this->middle(lb, rb); }
 
-// segment tree
-vector<int> cnt, len;
+    inline bool check(int l, int r) {
+        return l <= r && this->rb >= l && this->lb <= r;
+    }
 
-inline int lc(int p) {return p << 1;}
-inline int rc(int p) {return p << 1 | 1;}
-inline int middle(int l, int r) {return l+((r-l)>>1);}
+    inline bool contains(int l, int r) {
+        return l <= this->lb && r >= this->rb;
+    }
 
-void pushup(int p, int l, int r) {
-    if (cnt[p]) len[p] = disc[r+1] - disc[l];
-    else len[p] = (l == r) ? 0 : len[lc(p)] + len[rc(p)];
-}
+    template <typename... A>
+    inline static tree_ptr& allocate(tree_ptr& ptr, const A&... args) {
+        if (ptr == nullptr) ptr = std::make_unique<SegmentTree>(args...);
+        return ptr;
+    }
 
-void update(int s, int t, int k, int l, int r, int p) {
-    if (s <= l && t >= r) return cnt[p] += k, pushup(p, l, r);
-    auto mid = middle(l, r);
-    if (s <= mid) update(s, t, k, l, mid, lc(p));
-    if (t > mid) update(s, t, k, mid + 1, r, rc(p));
-    pushup(p, l, r);
-}
+    inline void allocate() {
+        auto mid = this->middle();
+        this->allocate(this->left, this->lb, mid);
+        this->allocate(this->right, mid + 1, this->rb);
+    }
 
-inline void update(int s, int t, int k) {update(s, t, k, 1, disc.size()-2, 1);}
+    // specialized push_up function
+    inline void push_up() {
+        if (tag) this->key = this->rb + 1 - this->lb;
+        else if (this->lb != this->rb) this->key = this->left->key + this->right->key;
+        else this->key = 0;
+    }
+
+    // specialized update function
+    inline void update(int k) {
+        this->tag = this->tag + k;
+        this->push_up();
+    }
+
+    // no need to push down
+
+    SegmentTree(int l, int r) : lb(l), rb(r) {}
+
+    void update(int l, int r, int k) {
+        if (!this->check(l, r)) return;
+        this->allocate();  // dynamic allocate
+        if (this->contains(l, r)) return this->update(k);
+        this->left->update(l, r, k), this->right->update(l, r, k);
+        this->push_up();
+    }
+};
+
+int n, min_pos, max_pos;
+long long ans;
+std::array<std::vector<std::tuple<int, int, int, int>>, 2> edges;
 
 signed main() {
 #ifndef ONLINE_JUDGE
     freopen("scanning-perimeter.in", "r", stdin);
 #endif
-    cin.tie(nullptr) -> sync_with_stdio(false);
-    int n; cin >> n;
-    vector<tuple<long long, int, int, int>> edge;
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cin >> n;
     for (int xa, ya, xb, yb; n; n--) {
-        cin >> xa >> ya >> xb >> yb;
-        edge.emplace_back(ya, 1, xa, xb);
-        edge.emplace_back(yb, -1, xa, xb);
-        disc.push_back(xa), disc.push_back(xb);
-    }
+        std::cin >> xa >> ya >> xb >> yb;
 
-    sort(edge.begin(), edge.end());
-    discrete();
+        // horizontal
+        edges[0].emplace_back(xa, -1, ya, yb);
+        edges[0].emplace_back(xb, 1, ya, yb);
 
-    cnt.resize(disc.size()*4), len.resize(disc.size()*4);
-    long long py = get<0>(edge[0]), sum = 0;
-    for (const auto& [y, w, xa, xb]: edge) {
-        sum += 2*(y-py) + len[1];
-        py = y;
-        update(pos(xa), pos(xb)-1, w);
+        // vertical
+        edges[1].emplace_back(ya, -1, xa, xb);
+        edges[1].emplace_back(yb, 1, xa, xb);
+
+        const auto pos = {xa, ya, xb, yb};
+        const auto& [min_ptr, max_ptr] = std::minmax_element(pos.begin(), pos.end());
+        min_pos = std::min(min_pos, *min_ptr);
+        max_pos = std::max(max_pos, *max_ptr);
     }
-    cout << sum << endl;
+    for (auto& arr : edges) std::sort(arr.begin(), arr.end());
+
+    SegmentTree tree(1, max_pos - min_pos + 1);
+    for (const auto& arr : edges) {
+        int tmp = 0;
+        for (const auto& [_, k, l, r] : arr) {
+            tree.update(l - min_pos + 1, r - min_pos, -k);
+            ans += std::abs(tree.key - tmp);
+            tmp = tree.key;
+        }
+    }
+    std::cout << ans << std::endl;
 }
