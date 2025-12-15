76700cec0a6fd261e81ae8c6c696b1fe98ad6e03
Julian Sun <101443258+chengzi-2333@users.noreply.github.com>
2025-12-15 16:43:02 +0800

    graph/flow(Dinic fixed)

--- template/graph/flow/max-flow/max-flow-augmenting.cpp
+++ template/graph/flow/max-flow/max-flow-augmenting.cpp
@@ -2,51 +2,14 @@
 #include <bits/stdc++.h>
 
 #define int long long
-#define MAX LONG_LONG_MAX
-
-using PII = std::pair<int, int>;
+#define INF LONG_LONG_MAX
 
 int n, m, s, t;
 std::vector<std::unordered_map<int, int>> g;
 
-int capacity() {
-    return std::accumulate(g[s].begin(), g[s].end(), static_cast<int>(0),
-                           [](int a, const PII& b) { return a + b.second; });
-}
-
-namespace FordFulkerson {
-
-std::vector<bool> vis;
-
-std::pair<bool, int> ford_fulkerson(int u, int neck) {
-    if (u == t) return {true, neck};
-    vis[u] = true;
-    bool flag = false;
-    for (auto& [v, w] : g[u]) {
-        if (w == 0 || vis[v]) continue;
-        std::tie(flag, neck) = ford_fulkerson(v, std::min(neck, w));
-        if (flag) {
-            g[v][u] += neck;
-            w -= neck;
-            break;
-        }
-    }
-    vis[u] = false;
-    return {flag, neck};
-}
-
-int ford_fulkerson() {
-    vis.resize(n + 1);
-    auto prev = capacity();
-    while (ford_fulkerson(s, MAX).first);
-    return prev - capacity();
-}
-
-}  // namespace FordFulkerson
-
 namespace EdmondsKarp {
 
-std::vector<int> shortest_path() {
+int augment() {
     std::vector<bool> vis(g.size());
     std::vector<int> fa(g.size());
     std::queue<int> que;
@@ -63,22 +26,25 @@ std::vector<int> shortest_path() {
             que.push(v);
         }
     }
-    return fa;
+    int neck = INF;
+    for (int u = fa[t], v = t; u; v = u, u = fa[u]) {
+        neck = std::min(neck, g[u][v]);
+    }
+    if (neck == INF) return 0;
+    for (int u = fa[t], v = t; u; v = u, u = fa[u]) {
+        g[v][u] += neck;
+        g[u][v] -= neck;
+    }
+    return neck;
 }
 
 int edmonds_karp() {
-    auto prev = capacity();
-    for (auto fa = shortest_path(); fa[t]; fa = shortest_path()) {
-        int neck = MAX;
-        for (int u = fa[t], v = t; u; v = u, u = fa[u]) {
-            neck = std::min(neck, g[u][v]);
-        }
-        for (int u = fa[t], v = t; u; v = u, u = fa[u]) {
-            g[v][u] += neck;
-            g[u][v] -= neck;
-        }
-    }
-    return prev - capacity();
+    int tmp = 0, flow = 0;
+    do {
+        tmp = augment();
+        flow += tmp;
+    } while (tmp);
+    return flow;
 }
 
 }  // namespace EdmondsKarp
@@ -87,7 +53,7 @@ namespace Dinic {
 
 std::vector<int> dep;
 
-bool bfs() {
+bool make_level() {
     dep.assign(g.size(), 0);
     std::queue<int> que;
     dep[s] = 1;
@@ -104,23 +70,33 @@ bool bfs() {
     return dep[t] != 0;
 }
 
-int dfs(int u, int resi) {
-    if (u == t || resi == 0) return resi;
-    for (auto& [v, w] : g[u]) {
-        if (dep[v] == dep[u] + 1 && (resi = dfs(v, std::min(resi, w)))) {
-            g[v][u] += resi;
-            w -= resi;
-            if (resi == 0) return resi;
-            // TODO
+int augment(int u, int fneck = INF) {
+    if (u == t || fneck == 0) return fneck;
+    for (const auto& [v, w] : g[u]) {
+        // TODO: current arc
+        if (dep[v] == dep[u] + 1 && w) {
+            auto neck = augment(v, std::min(fneck, w));
+            if (neck) {
+                g[v][u] += neck;
+                g[u][v] -= neck;
+                return neck;
+            } else {
+                dep[v] = 0;
+            }
         }
     }
-    return resi;
+    return 0;
 }
 
 int dinic() {
-    auto prev = capacity();
-    while (bfs()) dfs(s, MAX);
-    return prev - capacity();
+    int tmp = 0, flow = 0;
+    while (make_level()) {
+        do {
+            tmp = augment(s);
+            flow += tmp;
+        } while (tmp);
+    }
+    return flow;
 }
 
 }  // namespace Dinic
